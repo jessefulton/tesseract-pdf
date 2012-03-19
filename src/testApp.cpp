22e905e37730933bf6bc06aa9046c8da26839e7e
Jesse Fulton <jessefulton+github@gmail.com>
2012-03-19 15:08:36 -0700

    cleanup

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -44,15 +44,7 @@ void testApp::setup() {
 //--------------------------------------------------------------
 void testApp::update() {
 	mov.update();
-    //printf("" + mov.getCurrentFrame());
-    /*
-     if(panel.hasValueChanged("scale") || panel.hasValueChanged("medianSize")) {
-     ocrResult = runOcr(panel.getValueF("scale"), panel.getValueI("medianSize"));
-     scaled.update();
-     panel.clearAllChanged();
-     }
-     
-     */
+
 }
 
 
@@ -67,65 +59,10 @@ void testApp::draw() {
 
     scaled.draw(150,0);
 
-    
-    
-    /*
-     ofPushMatrix();
-     
-     ofTranslate(300, 0);
-     
-     ofSetColor(0);	
-     vector<string> lines = ofSplitString(ocrResult, "\n");
-     for(int i = 0; i < lines.size(); i++) {
-     ofDrawBitmapString(lines[i], 10, 20 + i * 12);
-     }
-     
-     ofSetColor(255);
-     img.draw(0, 200);
-     scaled.draw(0, 400);
-     
-     ofPopMatrix();
-     
-     
-     */
+
     
 }
 
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
 
 
 
@@ -172,14 +109,9 @@ void testApp::preprocessImage() {
     cv::medianBlur(scaled, medianSize);
 }
 
-// depending on the source of the text you want to OCR,
-// you might need to preprocess it. here i'm doing a
-// simple resize followed by a median blur.
 void testApp::runOcr() {
 	loadFrameToImage();
     preprocessImage();
-	//return tess.findText(scaled);
 	ocrResult = tess.findText(scaled);
     cout << ocrResult;
-    //return ocrResult;
 }
